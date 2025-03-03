ed587a0b50bb50a895414eb22f60e1f9e03a2e54
MJ KRUPAL <147424148+mjkrupal@users.noreply.github.com>
2025-03-03 15:42:03 +0530

    Add files via upload

---
+++ AdvancedCalculator.java
@@ -0,0 +1,15 @@
+package aec.edu.in;
+public class AdvancedCalculator
+{
+   public int findMaximum(int arr[])
+   {
+     int max=arr[0];
+      for(int i=1;i<arr.length;i++)
+      {
+            if(arr[i]>max)
+               max=arr[i];
+      }
+      return max;
+   }
+}
+
