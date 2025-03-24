bc5e28502e3e7d35a3e8d82fda8bec4471af410e
MJ KRUPAL <147424148+mjkrupal@users.noreply.github.com>
2025-03-24 09:38:58 +0530

    Add files via upload

---
+++ CommandlineProg1.java
@@ -0,0 +1,9 @@
+import java.lang.*;
+public class CommandlineProg1
+{
+    public static void main (String args[])
+    {
+        System.out.println(args[0]+args[1]);
+        System.out.println(args[1]);
+    }
+}
