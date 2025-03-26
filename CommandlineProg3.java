f76c1572bb1233125dd5af780dca28dc69cc6996
MJ KRUPAL <147424148+mjkrupal@users.noreply.github.com>
2025-03-26 20:27:47 +0530

    Add files via upload

---
+++ CommandlineProg3.java
@@ -0,0 +1,10 @@
+public class CommandlineProg3
+{
+    public static void main (String args[])
+    {
+        System.out.println("Emp ID: "+args[0]);
+        System.out.println("Emp Name: "+args[1]);
+        System.out.println("Designation: "+args[2]);
+        System.out.println("Company: "+args[3]);
+    }
+}
